dc0b1c192cc04a6abe45cb6682c948e17c5a7532
Victoria Dominguez Almela <vda1r22@soton.ac.uk>
2024-05-17 09:40:08 +0000

    version 1.0

---
+++ NEWS.md
@@ -0,0 +1,8 @@
+# bbnet 1.0
+
+bbnet 0.1 (Release date: 13 May 2024)
+==============
+
+Changes:
+
+* First version of the package
---
+++ inst/doc/bbnet.R
@@ -0,0 +1,47 @@
+## ----include = FALSE----------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ----setup--------------------------------------------------------------------
+library(bbnet)
+
+## -----------------------------------------------------------------------------
+data("my_BBN")
+
+head(my_BBN)
+
+## -----------------------------------------------------------------------------
+data("dogwhelk", "winkle", "combined")
+
+head(dogwhelk)
+
+## -----------------------------------------------------------------------------
+bbn.predict(bbn.model = my_BBN, priors1 = dogwhelk, figure = 0) # figure set to zero, this is explained below
+
+
+## -----------------------------------------------------------------------------
+bbn.predict(bbn.model = my_BBN, priors1 = dogwhelk, priors2 = winkle, priors3= combined, figure = 2, boot_max = 100, values = 0, font.size = 7)
+
+## -----------------------------------------------------------------------------
+bbn.timeseries(bbn.model = my_BBN, priors1 = combined, timesteps = 5, disturbance = 2)
+
+## -----------------------------------------------------------------------------
+bbn.visualise(bbn.model = my_BBN, priors1 = combined, timesteps = 5, disturbance = 2, threshold=0.05, font.size=0.7, arrow.size=4)
+
+## -----------------------------------------------------------------------------
+bbn.sensitivity(bbn.model = my_BBN, boot_max = 100, 'Limpet', 'Green Algae')
+
+## -----------------------------------------------------------------------------
+data("my_network")
+
+head(my_network)
+
+## -----------------------------------------------------------------------------
+bbn.network.diagram(bbn.network = my_network, font.size = 0.7, arrow.size = 4, arrange = layout_on_sphere)
+bbn.network.diagram(bbn.network = my_network, font.size = 0.7, arrow.size = 2, arrange = layout_on_grid)
+bbn.network.diagram(bbn.network = my_network, font.size = 0.7, arrow.size = 2, arrange = layout.random)
+bbn.network.diagram(bbn.network = my_network, font.size = 0.7, arrow.size = 2, arrange = layout.circle)
+
+
